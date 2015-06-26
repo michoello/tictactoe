06a1c70c0f9aefc0d3404fd8703c41471505d03c
azure provisioned user <azureuser@midolini.midolini.d1.internal.cloudapp.net>
2015-06-26 18:58:01 +0000

    minor

--- hello.py
+++ hello.py
@@ -1,33 +1,35 @@
 
 
 class Game:
-	def __init__(self, name):
-		self.field = [[0] * 6 for x in xrange(6)]
+    def __init__(self, name):
+        self.field = [[0] * 6 for x in xrange(6)]
 
-	def show(self):
-		print '----------------------'
-		for line in self.field:
-		    print '|', ''.join([ ' X ' if cell==1 else ' 0 ' if cell==2 else '   ' for cell in line]), \
-			      '|'
-		print '----------------------'
+    def show(self):
+        print '   ' + ' '.join([' '+str(x)+' ' for x in xrange(1,7)])
+        print '  -------------------------'
+        for idx, line in enumerate(self.field):
+            print idx + 1,  '|' + \
+                '|'.join([ ' X ' if cell==1 else ' O ' \
+                                 if cell==2 else '   ' for cell in line]) + \
+                '|'
+            print '  -------------------------'
+
+    def set(self, cell, val):
+        self.field[cell[0] - 1][cell[1] - 1] = 1 if val == 'X' else 2
 
 
-#		    for cell in line:
-#		        print ' X ' if cell == 1 else ' O ' if cell == 2 else '   '
 
 '''
 var = 'something'
 while var != '':
-	var = raw_input("Please enter something: ")
-	print "you entered", var
+    var = raw_input("Please enter something: ")
+    print "you entered", var
 '''
 
 game = Game('tictactoe')
 
-
-game.field[0][1] = 1
-game.field[2][3] = 2
-
+game.set((1,2), 'X')
+game.set((3,4), 'O')
 
 game.show()
 
