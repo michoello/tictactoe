e65a69c22ac8f563e08ba2e1ac4b0f78e9a9ccc8
azure provisioned user <azureuser@midolini.midolini.d1.internal.cloudapp.net>
2015-06-29 19:25:04 +0000

    stubborn player added

--- tictactoe.py
+++ tictactoe.py
@@ -3,41 +3,36 @@ import random
 # ---------------------------------------------------------------------
 class Game:
     def __init__(self):
-        self.field = [[0] * 6 for x in xrange(6)]
+        self.field = [[' '] * 6 for x in xrange(6)]
 
     def show(self):
         print '   ' + ' '.join([' '+str(x)+' ' for x in xrange(0,6)])
         print '  -------------------------'
         for idx, line in enumerate(self.field):
-            print idx,  '|' + \
-                '|'.join([ ' X ' if cell==1 else ' O ' \
-                                 if cell==2 else '   ' for cell in line]) + \
-                '|'
+            print idx,  '| ' + ' | '.join(line) + ' |'
             print '  -------------------------'
 
     def set(self, xy, val):
-        self.field[xy[0]][xy[1]] = 1 if val == 'X' else 2
+        self.field[xy[0]][xy[1]] = val
 
     def get(self, xy):
         x, y = xy
-        if 0 <= x <= 6 and 0 <= y <= 6:
-           val = self.field[x][y]
-           return 'X' if val == 1 else 'O' if val == 2 else ' '
-        return ' '
+        return self.field[x][y] if 0 <= x < 6 and 0 <= y < 6 else ' '
 
     def available(self):
-        return [(y, x) for y, row in enumerate(self.field) for x, v in enumerate(row) if v == 0]
+        return [(y, x) for y, row in enumerate(self.field) for x, v in enumerate(row) if v == ' ']
 
     def winner(self, player):
         n = 4
         str = player.role * n
-        for x in range(0, 6 - n + 1):
-            for y in range(0, 6 - n + 1):
+        for x in range(0, 6):
+            for y in range(0, 6):
                 hor = [(x, y + i) for i in range(0, n)]
                 ver = [(x + i, y) for i in range(0, n)]
-                dia = [(x + i, y + i) for i in range(0, n)]
+                dia1 = [(x + i, y + i) for i in range(0, n)]
+                dia2 = [(x + i, y - i) for i in range(0, n)]
 
-                hvd = [ ''.join([ self.get(xy) for xy in four ]) for four in [hor, ver, dia] ]
+                hvd = [ ''.join([ self.get(xy) for xy in four ]) for four in [hor, ver, dia1, dia2] ]
 
                 if str in hvd:
                     return True
@@ -84,15 +79,34 @@ class RandomPlayer(Player):
     def ply(self, game):
         return random.choice(game.available())
 
+# ---------------------------------------------------------------------
+class StubbornPlayer(Player):
+    def __init__(self, role):
+        Player.__init__(self, role)
+        self.__ply__ = self.__ply__(0, 0, [(dx,dy) for dx in range(0,6) for dy in range(0,6)])
+
+    def __ply__(self, x, y, steps):
+        for (dx, dy) in steps:
+            yield (x + dx, y + dy)
+
+    def ply(self, game):
+        while True:
+            step = next(self.__ply__)
+            if step in game.available():
+                return step
 
 # ---------------------------------------------------------------------
-def competition():
+# ---------------------------------------------------------------------
+def competition(show = False):
     results = {'X': 0, 'O': 0, ' ': 0}
     i = 0
     while i < 100:
+        playerX = RandomPlayer('X')
+        playerO = StubbornPlayer('O')
+
         i+=1
         tictactoe = Game()
-        winner = tictactoe.play([ RandomPlayer('X'), RandomPlayer('O') ], False)
+        winner = tictactoe.play([ playerX, playerO ], show)
         if winner is None:
             results[' '] = results[' '] + 1
         else:
@@ -102,15 +116,17 @@ def competition():
     print 'O: ', results['O']
     print ' : ', results[' ']
 
-
+# ---------------------------------------------------------------------
+def playwithme(roboPlayer = RandomPlayer('X')):    
+    tictactoe = Game()
+    winner = tictactoe.play([ roboPlayer, HumanPlayer('O') ], True)
+    print 'Winner: ', winner.role
 
 
 # ---------------------------------------------------------------------
+#playwithme(StubbornPlayer('X'))
 
-# competition()
+competition(show=False)
 
-tictactoe = Game()
-winner = tictactoe.play([ RandomPlayer('X'), HumanPlayer('O') ], True)
 
-print 'Winner: ', winner.role
 
