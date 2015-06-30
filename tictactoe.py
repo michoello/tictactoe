4ce3e18691687bd1726a4b42f50331d147b06d8e
azure provisioned user <azureuser@midolini.midolini.d1.internal.cloudapp.net>
2015-06-30 01:34:02 +0000

    network added

--- tictactoe.py
+++ tictactoe.py
@@ -1,12 +1,16 @@
 import random
+import network
+
+net = network.Network([72, 36, 16, 2])
 
 # ---------------------------------------------------------------------
 class Game:
     def __init__(self):
-        self.field = [[' '] * 6 for x in xrange(6)]
+        self.field = [[' '] * 6 for x in range(6)]
+        self.history = []
 
     def show(self):
-        print '   ' + ' '.join([' '+str(x)+' ' for x in xrange(0,6)])
+        print '   ' + ' '.join([' '+str(x)+' ' for x in range(0,6)])
         print '  -------------------------'
         for idx, line in enumerate(self.field):
             print idx,  '| ' + ' | '.join(line) + ' |'
@@ -17,7 +21,7 @@ class Game:
 
     def get(self, xy):
         x, y = xy
-        return self.field[x][y] if 0 <= x < 6 and 0 <= y < 6 else ' '
+        return self.field[x][y] if x in range(6) and y in range(6) else ' '
 
     def available(self):
         return [(y, x) for y, row in enumerate(self.field) for x, v in enumerate(row) if v == ' ']
@@ -25,12 +29,12 @@ class Game:
     def winner(self, player):
         n = 4
         str = player.role * n
-        for x in range(0, 6):
-            for y in range(0, 6):
-                hor = [(x, y + i) for i in range(0, n)]
-                ver = [(x + i, y) for i in range(0, n)]
-                dia1 = [(x + i, y + i) for i in range(0, n)]
-                dia2 = [(x + i, y - i) for i in range(0, n)]
+        for x in range(6):
+            for y in range(6):
+                hor = [(x, y + i) for i in range(n)]
+                ver = [(x + i, y) for i in range(n)]
+                dia1 = [(x + i, y + i) for i in range(n)]
+                dia2 = [(x + i, y - i) for i in range(n)]
 
                 hvd = [ ''.join([ self.get(xy) for xy in four ]) for four in [hor, ver, dia1, dia2] ]
 
@@ -42,16 +46,40 @@ class Game:
         return not self.available()
 
     def play(self, players, show):
-        while True: 
+        winner = None 
+        history = []
+        gameOver = False
+        while not gameOver: 
            for player in players:
+
                xy = player.ply(self)
                self.set(xy, player.role)
+              
+               history.append(self.plainarray())
  
-               if show: self.show()
+               if show:
+                   self.show()
 
-               if self.winner(player): return player
-               if self.draw(): return None
+               if self.winner(player): 
+                   gameOver, winner = True, player
+                   break
 
+               if self.draw():
+                   gameOver = True
+                   break
+        
+        if winner != None:
+           y0 = 1 if winner.role == 'X' else 0
+           for idx, position in enumerate(reversed(history)):
+               net.train(position, [y0, 1.0/(idx+1)], 0.5, 10/(idx+1))
+               # print
+               # print 'Position:', idx + 1, ' -> ', position
+               # print 'Predict: ', net.predict(position)
+
+        return winner
+
+    def plainarray(self):
+        return sum( map( lambda c : [int(c == 'X'), int(c == 'O')], [ c for row in self.field for c in row ]), [])
 
 # ---------------------------------------------------------------------
 class Player:
@@ -83,7 +111,7 @@ class RandomPlayer(Player):
 class StubbornPlayer(Player):
     def __init__(self, role):
         Player.__init__(self, role)
-        self.__ply__ = self.__ply__(0, 0, [(dx,dy) for dx in range(0,6) for dy in range(0,6)])
+        self.__ply__ = self.__ply__(0, 0, [(dx,dy) for dx in range(6) for dy in range(6)])
 
     def __ply__(self, x, y, steps):
         for (dx, dy) in steps:
@@ -97,12 +125,12 @@ class StubbornPlayer(Player):
 
 # ---------------------------------------------------------------------
 # ---------------------------------------------------------------------
-def competition(show = False):
+def competition(show = False, rounds = 100):
     results = {'X': 0, 'O': 0, ' ': 0}
     i = 0
-    while i < 100:
-        playerX = RandomPlayer('X')
-        playerO = StubbornPlayer('O')
+    while i < rounds:
+        playerX, playerO = StubbornPlayer('X'), RandomPlayer('O')
+        #playerX, playerO = RandomPlayer('X'), StubbornPlayer('O')
 
         i+=1
         tictactoe = Game()
@@ -126,7 +154,7 @@ def playwithme(roboPlayer = RandomPlayer('X')):
 # ---------------------------------------------------------------------
 #playwithme(StubbornPlayer('X'))
 
-competition(show=False)
+competition(show=True, rounds = 1)
 
 
 
